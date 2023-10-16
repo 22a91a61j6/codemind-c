09ecfa92a9c68e50cca2dbecccb46d75fddbf48c
YELISETTI S R K SURYA CHANDRA RAJESH <121783995+22a91a61j6@users.noreply.github.com>
2023-10-16 21:46:05 +0530

    #include <stdio.h>   int main(){     // public clss Test{     // public static void main(String []args){     //     Scanner sc=new Scanner(System.in);     //     char c=sc.next(). charAt(0);     char c;     scanf("%c",&c);         if(c=='V' || c=='v'){             printf("Violet");         }else if(c=='I' || c=='i'){             printf("Indigo");         }else if(c=='B' || c=='b'){             printf("Blue");         }else if(c=='G' || c=='g'){             printf("Green");         }else if(c=='Y' || c=='y'){             printf("Yellow");         }else if(c=='O' || c=='o'){             printf("Orange");         }else if(c=='R' || c=='r'){             printf("Red");         }else{             printf("-1");         }
        }
    // }
    // }

---
+++ Print_the_color_name.c
@@ -0,0 +1,29 @@
+  #include <stdio.h>
+  int main(){
+    // public clss Test{
+    // public static void main(String []args){
+    //     Scanner sc=new Scanner(System.in);
+    //     char c=sc.next(). charAt(0);
+    char c;
+    scanf("%c",&c);
+        if(c=='V' || c=='v'){
+            printf("Violet");
+        }else if(c=='I' || c=='i'){
+            printf("Indigo");
+        }else if(c=='B' || c=='b'){
+            printf("Blue");
+        }else if(c=='G' || c=='g'){
+            printf("Green");
+        }else if(c=='Y' || c=='y'){
+            printf("Yellow");
+        }else if(c=='O' || c=='o'){
+            printf("Orange");
+        }else if(c=='R' || c=='r'){
+            printf("Red");
+        }else{
+            printf("-1");
+        }
+        
+    }
+// }
+// }
