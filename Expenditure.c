b05bb7d3e25722898f71231feed8af25a8bda012
YELISETTI S R K SURYA CHANDRA RAJESH <121783995+22a91a61j6@users.noreply.github.com>
2023-10-17 18:32:38 +0530

    #include<stdio.h> int main(){
    //     import java.util.*;
    // public class Tst{
    //     public static void main(String []args){
    //         Scanner sc=new Scanner(System.in);
    //         int x=sc.nextInt();
    //         int y=sc.nextInt();
        int x,y;
        scanf("%d%d",&x,&y);
            if(y*30 <=x){
                //System.out.println("YES");
                printf("YES");
            }
            else{
                //System.out.println("NO");
                printf("NO");
            }
        }
    //

---
+++ Expenditure.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){
+    
+//     import java.util.*;
+// public class Tst{
+//     public static void main(String []args){
+//         Scanner sc=new Scanner(System.in);
+//         int x=sc.nextInt();
+//         int y=sc.nextInt();
+    int x,y;
+    scanf("%d%d",&x,&y);
+        if(y*30 <=x){
+            //System.out.println("YES");
+            printf("YES");
+        }
+        else{
+            //System.out.println("NO");
+            printf("NO");
+        }
+    }
+//
+
