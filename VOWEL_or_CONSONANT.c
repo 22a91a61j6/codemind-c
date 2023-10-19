99d179b5ab7ed64947cd8056202ca45f8e96e062
YELISETTI S R K SURYA CHANDRA RAJESH <121783995+22a91a61j6@users.noreply.github.com>
2023-10-19 10:35:35 +0530

    #include<stdio.h> int main(){     // import java.util.*;     // public class Test{     // public static void main(String []args){     //     Scanner sc=new Scanner(System.in);     char s;     scanf("%c",&s);//=sc.next().charAt(0);         if(s=='a' ||s=='e' ||s=='i' ||s=='o' ||s=='u' ||s=='A' ||s=='E' ||s=='I' ||s=='O' ||s=='U'){             printf("VOWEL");         }else{             printf("CONSONANT");         } //     } // } }

---
+++ VOWEL_or_CONSONANT.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    // import java.util.*;
+    // public class Test{
+    // public static void main(String []args){
+    //     Scanner sc=new Scanner(System.in);
+    char s;
+    scanf("%c",&s);//=sc.next().charAt(0);
+        if(s=='a' ||s=='e' ||s=='i' ||s=='o' ||s=='u' ||s=='A' ||s=='E' ||s=='I' ||s=='O' ||s=='U'){
+            printf("VOWEL");
+        }else{
+            printf("CONSONANT");
+        }
+//     }
+// }
+}
