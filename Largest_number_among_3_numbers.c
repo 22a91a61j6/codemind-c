8c1bc6e97b72a15b0fad242a9ebdbfcdfd5a99d8
YELISETTI S R K SURYA CHANDRA RAJESH <121783995+22a91a61j6@users.noreply.github.com>
2023-10-21 19:47:41 +0530

    #include<stdio.h> int main(){
    //     import java.util.*;
    // public class Test{
    //     public static void main(String []args){
    //         Scanner sc=new Scanner(System.in);
        int a,b,c;
        scanf("%d%d%d",&a,&b,&c);
            // int b=sc.nextInt();
            // int c=sc.nextInt();
            if(a>b && a>c){
               printf("%d",a);
            }else if(b>a && b>c){
                printf("%d",b);
            }else{
                printf("%d",c);
             }
    //     }
        
    // }
    }

---
+++ Largest_number_among_3_numbers.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){
+    
+//     import java.util.*;
+// public class Test{
+//     public static void main(String []args){
+//         Scanner sc=new Scanner(System.in);
+    int a,b,c;
+    scanf("%d%d%d",&a,&b,&c);
+        // int b=sc.nextInt();
+        // int c=sc.nextInt();
+        if(a>b && a>c){
+           printf("%d",a);
+        }else if(b>a && b>c){
+            printf("%d",b);
+        }else{
+            printf("%d",c);
+         }
+//     }
+    
+// }
+}
