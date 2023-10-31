fa3f1e91c263ac34782b6d73480eb28a4b99a4f1
YELISETTI S R K SURYA CHANDRA RAJESH <121783995+22a91a61j6@users.noreply.github.com>
2023-10-31 11:38:26 +0530

    #include<stdio.h> int main(){ //     import java.util.*; // public class Test{ //     public static void main(String[] args){ //         Scanner sc=new Scanner(System.in);     int n,i;     scanf("%d",&n);     for(i=1;i<=n;i++){         printf("%d ",i);     } //     }
    // }
    }

---
+++ N_natural_numbers.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+//     import java.util.*;
+// public class Test{
+//     public static void main(String[] args){
+//         Scanner sc=new Scanner(System.in);
+    int n,i;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++){
+        printf("%d ",i);
+    }
+//     }
+    
+// }
+}
