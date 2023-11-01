27a1c8d1cd071ac33188106d6ad31af349c09d01
YELISETTI S R K SURYA CHANDRA RAJESH <121783995+22a91a61j6@users.noreply.github.com>
2023-11-01 11:35:32 +0530

    #include<stdio.h> int main(){ //     import java.util.*; // public class Test{ //     public static void main(String[] args){ //         Scanner sc=new Scanner(System.in); //         int n=sc.nextInt();     int n;     scanf("%d",&n);         for(int i=1;i<=n;i++){             if(i%2!=0){                 printf("%d ",i);             }         } //     } // } }

---
+++ Odd Natural_Numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+//     import java.util.*;
+// public class Test{
+//     public static void main(String[] args){
+//         Scanner sc=new Scanner(System.in);
+//         int n=sc.nextInt();
+    int n;
+    scanf("%d",&n);
+        for(int i=1;i<=n;i++){
+            if(i%2!=0){
+                printf("%d ",i);
+            }
+        }
+//     }
+// }
+}
